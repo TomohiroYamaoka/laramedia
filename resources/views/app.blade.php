27cc0b171b804356188d0d541c3f46760a6ccffd
山岡智裕 <>
2020-05-18 18:46:58 +0900

    フロントの変更

---
+++ resources/views/app.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <!-- レスポンシブに対応-->
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>
+  @yield('title')
+  </title>
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+  <!-- Material Design Bootstrap -->
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.8.11/css/mdb.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="/css/styles.css">
+  <!-- bootstrap.cssの使用 -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+</head>
+
+<body>
+
+  @yield('content')
+
+  <!-- JQuery -->
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <!-- Bootstrap tooltips -->
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js"></script>
+  <!-- Bootstrap core JavaScript -->
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
+  <!-- MDB core JavaScript -->
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.8.11/js/mdb.min.js"></script>
+</body>
--- resources/views/main/index.blade.php
+++ resources/views/main/index.blade.php
@@ -1,110 +1,33 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>ToDo App</title>
-  <link rel="stylesheet" href="/css/styles.css">
-</head>
-　
-<body>
-    <section id="header">
-        <div class="container">
-            <img src="/images/reading.png" class="logo">
-            <div class="header-text">
-                <h1>The purpose is to teach, bring</h1>
-                <span class="square"></span>
-                <p>ffajknfa fajkfa;fa afaj;</p>
-                <button class="common-btn">Read more</button>
-            </div>
-        </div>
-    </section> 
-        
-   <!-------about------------> 
-   <section id="about">
-        <div class="about-left-col">
-           <img src="images/about.png">
-        </div>   
-        <div class="about-right-col">
-            <div class="about-text">
-                <h1>About this media</h1>
-                <p>このアプリの説明文章<p>     
-            </div>        
-        </div>    
-    </section>
-    
-    <!-------features------------> 
-    <section id="features">
-        <div class="feature-row">
-            <div class="feature-col">
-                <img src="images/feature1.png">
-                <h4>Learn Anyywhere</h4>
-                <p>fafauhafa</p>
-            </div>
-            <div class="feature-col">
-                <img src="images/features2.png">
-                <h4>Expert Teachers</h4>
-                <p>fafjajfai</p>
-            </div>
-            <div class="feature-col">
-                <img src="images/features2.png">
-                <h4>Expert Teachers</h4>
-                <p>fafjajfai</p>
-            </div>
-        </div>
-        <div class="feature-btn">
-            <button class="common-btn">Read More</button>
-        </div>    
-    </section> 
-    
-    
+@extends('app')
 
-    <!-------courses------------> 
-    <section id="courses">
-        <div class="container course-row">
-            <div class="course-left-col">
-                <div class="course-text">
-                    <h1>Browse our top</h1>
-                </div>                
-                    <div class="course-right-col">
-                <img src="images/course.png">
-            </div>
-        </div>   
-    </section>         
+@section('title', '記事一覧')
 
-    
-    
-    
-    
-    
-    <!------サイドバー----->
-     <nav id="sideNav">
-         <ul>
-             <li><a href="#">HOME</a></li>
-             <li><a href="#">ABOUT US</a></li>
-             <li><a href="#">FEATURES</a></li>
-             <li><a href="#">COURSES</a></li>
-             <li><a href="#">OFFER</a></li>
-             <li><a href="#">CONTACT</a></li>
-         </ul>
-        </nav>
-    <img src="images/menu.png" id="menuBtn">    
-    
-    <!-------サイドバーのアニメーション----------->
-    <script>
-        var menuBtn=document.getElementById("menuBtn")
-        var sideNav=document.getElementById("sideNav")
+@section('content')
+@include('nav')
+<!-- Card -->
+<div class="card">
+  <!-- Card image -->
+  <div class="view overlay">
+    <img class="card-img-top" src="https://mdbootstrap.com/img/Mockups/Lightbox/Thumbnail/img%20(67).jpg"
+      alt="Card image cap">
+      <a href="#!">
+      <div class="mask rgba-white-slight"></div>
+      </a>
+    </div>
 
-        sideNav.style.right="-250px"
-        menuBtn.onclick=function(){
-            if(sideNav.style.right=="-250px"){
-                sideNav.style.right="0";
-            }
-            else{
-                sideNav.style.right="-250px";
-            }
-        }
-    </script>    
-</body> 
+  <!-- Card content -->
+  <div class="card-body">
 
+    <!-- Title -->
+    <h4 class="card-title">Card title</h4>
+    <!-- Text -->
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+      content.</p>
+    <!-- Button -->
+    <a href="#" class="btn btn-primary">Button</a>
+
+  </div>
+
+</div>
+<!-- Card -->
+@endsection
