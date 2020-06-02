7a2aaef420be45b5e962fc8a7127e570ef827844
山岡智裕 <>
2020-06-02 16:55:02 +0900

    色を統一する

--- resources/views/nav.blade.php
+++ resources/views/nav.blade.php
@@ -1,5 +1,5 @@
 <!--Navbar -->
-<nav class="mb-1 navbar navbar-expand-lg navbar-dark default-color">
+<nav class="mb-1 navbar navbar-expand-lg navbar-dark  deep-blue-gradient">
   <a class="navbar-brand" href="#">Venect Intern Media</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-333"
     aria-controls="navbarSupportedContent-333" aria-expanded="false" aria-label="Toggle navigation">
@@ -7,8 +7,8 @@
   </button>
   <div class="collapse navbar-collapse" id="navbarSupportedContent-333">
     <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home
+      <li class="nav-item ">
+        <a class="nav-link" href="#">ホーム
           <span class="sr-only">(current)</span>
         </a>
       </li>
