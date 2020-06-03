9c4ecbbdfccecddac0b7a9a2a47c30a24cbae186
山岡智裕 <>
2020-06-03 21:56:56 +0900

    記事更新。削除機能の追加

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -20,10 +20,59 @@
                 　　　　　　<img class="card-img-top" src="./assets/index.jpg" alt="Card image cap">
                 　　　　　　　<a><div class="mask rgba-white-slight"></div></a>
                 　　　　</div>
+                @if( Auth::id() === $article->user_id )
+                <!-- dropdown -->
+                <div class="ml-auto card-text">
+                <div class="dropdown">
+                　　<a data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                <button type="button" class="btn btn-link text-muted m-0 p-2">
+                  <i class="fas fa-ellipsis-v"></i>
+                </button>
+              </a>
+              <div class="dropdown-menu dropdown-menu-right">
+                <a class="dropdown-item" href="{{ route("articles.edit", ['article' => $article]) }}">
+                  <i class="fas fa-pen mr-1"></i>記事を更新する
+                </a>
+                <div class="dropdown-divider"></div>
+                <a class="dropdown-item text-danger" data-toggle="modal" data-target="#modal-delete-{{ $article->id }}">
+                  <i class="fas fa-trash-alt mr-1"></i>記事を削除する
+                </a>
+              </div>
+            </div>
+          </div>
+          <!-- dropdown -->
+  
+          <!-- modal -->
+          <div id="modal-delete-{{ $article->id }}" class="modal fade" tabindex="-1" role="dialog">
+            <div class="modal-dialog" role="document">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <button type="button" class="close" data-dismiss="modal" aria-label="閉じる">
+                    <span aria-hidden="true">×</span>
+                  </button>
+                </div>
+                <form method="POST" action="{{ route('articles.destroy', ['article' => $article]) }}">
+                  @csrf
+                  @method('DELETE')
+                  <div class="modal-body">
+                    {{ $article->title }}を削除します。よろしいですか？
+                  </div>
+                  <div class="modal-footer justify-content-between">
+                    <a class="btn btn-outline-grey" data-dismiss="modal">キャンセル</a>
+                    <button type="submit" class="btn btn-danger">削除する</button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+          <!-- modal -->
+        @endif
+        {{-- ここまで追加 --}}
                 　　　　　<!-- Card content -->
                 　　　　<div class="card-body card-body-cascade">
                 　　　　　<!-- Label -->
                 　　　　　　<h5 class="pink-text pb-2 pt-1"><i class="fas fa-utensils"></i>{{ $article->user->name }} {{ $article->created_at->format('Y/m/d H:i') }} </h5>
+ 
                 　　　　　　<!-- Title -->
                 　　　　　　　<h4 class="font-weight-bold card-title">{{ $article->title }}</h4>
                 　　　　　　　　　<!-- Text -->
